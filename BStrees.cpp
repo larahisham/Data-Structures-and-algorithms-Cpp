3d4ebd1fc8f0c8f6588561f280ed6f17e8b82d08
Lara Hisham <145819936+larahisham@users.noreply.github.com>
2024-02-10 02:54:23 +0300

    Create BStrees.cpp
    Signed-off-by: Lara Hisham <145819936+larahisham@users.noreply.github.com>

---
+++ BStrees.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std ;
+class Node {
+public :
+Node * right ;
+Node * left ;
+int value ;
+Node (int value){
+  this - > value = value ;
+  right = nullptr;
+  left = nullptr;
+    }
+};
+
+class Bstree {
+public : 
+Node * root ;
+Bstrees(int value){
+  root = nullptr ;
+  } 
+bool Insertnode(int value){
+  Node * Newnode = new Node (value);
+  if (root == nullptr){
+    root = Newnode;
+    return true ;
+   } Node * temp = root;
+  while (true){
+    if (Newnode -> value == temp -> value) return false ;
+      if (Newnode -> value < temp -> value ){
+        if (temp -> left == nullptr){
+          temp -> left = Newnode ;
+          return true;
+          }
+        temp = temp -> left
+        } else {
+          if (temp -> right == nullptr){
+            temp -> right = Newnode ;
+            return true;
+          }
+        temp = temp -> right;
+        }
+     }
+  }
+
+bool Contains (int value){
+  Node * temp = root ;
+  while (temp) {
+    if (value < temp -> value){
+      temp = temp -> left ;
+     } else if (value > temp -> value){
+      temp = temp -> right ;
+     } else {
+      return true;
+     }
+   }
+  return false ;
+ }
+
+};
