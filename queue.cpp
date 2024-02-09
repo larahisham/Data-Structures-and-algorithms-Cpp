bba8ada54061f1dceb1758d4b7b139c404a09437
Lara Hisham <145819936+larahisham@users.noreply.github.com>
2024-02-09 23:26:38 +0300

    Update queue.cpp
    Signed-off-by: Lara Hisham <145819936+larahisham@users.noreply.github.com>

--- queue.cpp
+++ queue.cpp
@@ -1,2 +1,51 @@
 #include <iostream>
 using namespace std ;
+class Node {
+public : 
+int value ;
+Node * next ;
+ Node (int value){
+   this -> value = value ;
+   next = nullptr;
+  }
+};
+
+class Queue {
+private : 
+Node * first ;
+Node * last ;
+int length = 0 ;
+Queue (int value) {
+  Node * Newnode = new Node (value);
+  first = Newnode ;
+  last = Newnode ;
+  length ++;
+  }
+
+
+void Enqueue (int value) {
+  Node * Newnode = new Node (value);
+  if (length == 0) {
+    first = Newnode;
+    last = Newnode ;
+  } else {
+    last -> next = Newnode ;
+    last = Newnode ;
+    }
+  length++;
+}
+
+void Dequeue () {
+  if (length == 0) return;
+  Node * temp = first ;
+  if (length == 1){
+    first = nullptr;
+    last = nullptr;
+  } else {
+    first = first -> next ;
+  } 
+  delete temp ;
+  length --;
+  return ;
+  }
+}; // Lara H. A B Y 
