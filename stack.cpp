2271f4a9fbd9a7b2294909269baa64eac3f52e5f
Lara Hisham <145819936+larahisham@users.noreply.github.com>
2024-02-09 22:08:22 +0300

    Update stack.cpp
    Signed-off-by: Lara Hisham <145819936+larahisham@users.noreply.github.com>

--- stack.cpp
+++ stack.cpp
@@ -1,2 +1,41 @@
 #include <iostream>
 using namespace std ;
+
+class Node {
+public : 
+int value ;
+Node * next ;
+ Node (int value){
+   this -> value = value ;
+   next = nullptr;
+ }
+};
+
+class Stack {
+private :
+Node * top;
+int height;
+public :
+stack (int value){
+  Node * Newnode = new Node (value);
+  top = Newnode ;
+  height = 1;
+  }
+void Push (int value){
+  Node * Newnode = new Node (value);
+  if (height == 0) 
+    top = Newnode ; 
+  else {
+    Newnode -> next = top;
+    top = Newnode;
+  }
+}
+void Printstack (){
+  Node * temp = top;
+  while (temp){
+    cout << temp -> value << "  ";
+    temp = temp -> next ;
+  }
+}
+
+};
