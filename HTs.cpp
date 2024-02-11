e47297464c3b4d1742ab8310dbbd527781ad0825
Lara Hisham <145819936+larahisham@users.noreply.github.com>
2024-02-11 03:05:58 +0300

    Update HTs.cpp
    Signed-off-by: Lara Hisham <145819936+larahisham@users.noreply.github.com>

--- HTs.cpp
+++ HTs.cpp
@@ -1,4 +1,67 @@
 #include <iostream>
+#include <string>
 #include <cmath>
 using namespace std ;
-class HTs 
+class Node {
+public :
+string key;
+int value;
+Node * next;
+Node (string key , int value){
+  this -> key = key ;
+  this -> value = value;
+  next = nullptr;
+    }
+};
+
+class HTs {
+private :
+static const int size = 7;
+Node * Datamap[size];
+public :
+void Print (){
+  for (int i = 0; i< size ; i++){
+    cout << i << "=" <<"\n";
+    if (Datamap[i]){
+      Node* temp = Datamap[i];
+      while(temp){
+        cout <<"(" << temp -> key << "," << temp -> value << ")" ;
+        temp = temp -> next ;
+           }
+        }
+      }
+   }
+
+int Hash (string key) {
+  int hash = 0;
+  for (int i = 0 ; i < key.length();i++){
+     int ASCII4V = int (key[i]);
+     hash = (hash + ASCII4V*7) % size ;
+    }
+  return hash ;
+  }
+
+void Setnodes (string key , int value){
+  int index = Hash(key);
+  Node * Newnode = new Node (key , value);
+  if (Datamap[index] == nullptr){
+    Datamap[index] = Newnode;
+  } else {
+    Node * temp = Datamap[index];
+    while(temp-> next != nullptr){
+      temp = temp -> next ;
+    } temp -> next - Newnode;
+  }
+}
+
+int Getnode (string key){
+  int index = Hash(key);
+  Node* temp = Datamap[index];
+  while (temp != nullptr){
+    if (temp -> key == key) return temp-> value;
+    temp = temp -> next ;
+      }
+  return 0 ;
+   }
+
+};
